21729d338238531cdd6ac4ed82b1d8e9747beac1
mohab ali <mohabzalat20@gmail.com>
2025-09-01 14:02:53 +0300

    feat : adding insertion sort example

---
+++ data_structure&algorithms_by_mark_weiss/part_2_algorithms&building_blocks/sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void insertionSort(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+int main()
+{
+    vector arr = {1, 2, 4, 7, 98, 3};
+    insertionSort(arr);
+
+    for (int i : arr)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}

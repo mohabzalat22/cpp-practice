2d8da5b073516e054bec09b508f89b0a0b161e2d
mohabzalat22 <mohabzalat20@gmail.com>
2026-07-16 18:45:52 +0300

    solve next round

---
+++ codeforces/800/nextRound.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> getTopX(vector<int> l, int n){
+    vector<int> list;
+    for(int i=0;i<n;i++){
+        list.push_back(l[i]);
+    }
+    return list;
+};
+
+int main(){
+
+    int n, k;
+    cin>> n >>k;
+
+    vector<int> list;
+    int nextRound=0;
+    
+    while(n--){
+        int score;
+        cin >> score;
+        list.push_back(score);
+    }
+
+    for(int i=0; i<list.size();i++){
+        vector top = getTopX(list,k);
+        auto found = find(top.begin(), top.end(), list[i]);
+
+        if(found != top.end()){
+            int index = found - top.begin();
+            if(index != -1){
+                nextRound ++;
+            }
+        }
+    }
+    cout << nextRound;
+
+    return 0;
+}
